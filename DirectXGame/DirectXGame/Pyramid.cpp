c812f0008083951daaa509f1ca7935d2a7fd2560
KyleDesquitado <kyle_francis_desquitado@dlsu.edu.ph>
2022-11-12 15:52:11 +0800

    added bounding box for pyramid

--- DirectXGame/DirectXGame/Pyramid.cpp
+++ DirectXGame/DirectXGame/Pyramid.cpp
@@ -45,9 +45,39 @@ Pyramid::Pyramid(string name) :AGameObject(name)
 		3,4,0
 	};
 
+	Vertex vertex_list_box[] =
+	{
+		{Vector3D(-0.5f,-0.5f,-0.5f),	Vector3D(1,0,0), Vector3D(0.2f,0,0) },
+		{Vector3D(0.0f,0.5f,0.0f),    Vector3D(1,0,0), Vector3D(0.2f,0.2f,0) },
+		{Vector3D(0.5f,-0.5f,-0.5f),     Vector3D(1,0,0),  Vector3D(0.2f,0.2f,0) },
+		{Vector3D(0.5f,-0.5f,0.5f),     Vector3D(1,0,0), Vector3D(0.2f,0,0) },
+		{Vector3D(-0.5f,-0.5f,0.5f),	Vector3D(1,0,0), Vector3D(0,0.2f,0) }
+	};
+
+	unsigned int index_line_list[] =
+	{
+		1, 2,
+		1, 0,
+		0, 2,
+
+		2, 3,
+		1, 3,
+
+		3, 4,
+		1, 4,
+
+		4, 0,
+
+		0, 3
+	};
+
 	//Index Buffer
 	this->indexBuffer = GraphicsEngine::get()->createIndexBuffer();
 	this->indexBuffer->load(index_list, ARRAYSIZE(index_list));
+
+	//Index Buffer
+	this->boxIndexBuffer = GraphicsEngine::get()->createIndexBuffer();
+	this->boxIndexBuffer->load(index_line_list, ARRAYSIZE(index_line_list));
 
 	//Vertex Shader
 	GraphicsEngine::get()->compileVertexShader(L"VertexShader.hlsl", "vsmain", &shaderByteCode, &sizeShader);
@@ -59,6 +89,9 @@ Pyramid::Pyramid(string name) :AGameObject(name)
 
 	this->gizmoVertexBuffer = GraphicsEngine::get()->createVertexBuffer();
 	this->gizmoVertexBuffer->load(vertex_list_gizmo, sizeof(Vertex), ARRAYSIZE(vertex_list_gizmo), shaderByteCode, sizeShader);
+
+	this->boxVertexBuffer = GraphicsEngine::get()->createVertexBuffer();
+	this->boxVertexBuffer->load(vertex_list_box, sizeof(Vertex), ARRAYSIZE(vertex_list_box), shaderByteCode, sizeShader);
 
 	GraphicsEngine::get()->releaseCompiledShader();
 
@@ -74,6 +107,8 @@ Pyramid::Pyramid(string name) :AGameObject(name)
 	this->constantBuffer->load(&cbData, sizeof(CBData));
 
 	setAnimSpeed(4);
+
+	boundBoxScale = Vector3D(1.1, 1.1, 1.1);
 }
 
 Pyramid::~Pyramid()
@@ -132,7 +167,7 @@ void Pyramid::draw(int width, int height)
 	cbData.viewMatrix = cameraMatrix;
 
 	//Perspective View
-	cbData.projMatrix.setPerspectiveFovLH(1.57f, ((float)width / (float)height), 0.001f, 1000.0f);
+	cbData.projMatrix.setPerspectiveFovLH(1.57f, ((float)width / (float)height), 0.1f, 100.0f);
 
 	this->constantBuffer->update(deviceContext, &cbData);
 
@@ -147,6 +182,7 @@ void Pyramid::draw(int width, int height)
 	deviceContext->setIndexBuffer(this->indexBuffer);
 	deviceContext->setVertexBuffer(this->vertexBuffer);
 
+	GraphicsEngine::get()->getImmediateDeviceContext()->setSolidRenderMode();
 	deviceContext->drawIndexedTriangleList(this->indexBuffer->getSizeIndexList(), 0, 0);
 }
 
@@ -207,7 +243,70 @@ void Pyramid::drawGizmo(int width, int height)
 	deviceContext->setIndexBuffer(this->indexBuffer);
 	deviceContext->setVertexBuffer(this->gizmoVertexBuffer);
 
+	GraphicsEngine::get()->getImmediateDeviceContext()->setSolidRenderMode();
 	deviceContext->drawIndexedTriangleList(this->indexBuffer->getSizeIndexList(), 0, 0);
+}
+
+void Pyramid::drawBox(int width, int height)
+{
+	GraphicsEngine* graphEngine = GraphicsEngine::get();
+	DeviceContext* deviceContext = graphEngine->getImmediateDeviceContext();
+
+	CBData cbData = {};
+
+	cbData.time = deltaTime;
+
+	//Add object transformation
+	Matrix4x4 temp;
+
+	cbData.worldMatrix.setIdentity();
+
+	Matrix4x4 world_cam;
+	world_cam.setIdentity();
+
+	temp.setIdentity();
+	temp.setScale(boundBoxScale);
+	cbData.worldMatrix *= temp;
+
+	temp.setIdentity();
+	temp.setRotationX(getLocalRotation().m_x);
+	cbData.worldMatrix *= temp;
+
+	temp.setIdentity();
+	temp.setRotationY(getLocalRotation().m_y);
+	cbData.worldMatrix *= temp;
+
+	temp.setIdentity();
+	temp.setRotationZ(getLocalRotation().m_z);
+	cbData.worldMatrix *= temp;
+
+	temp.setIdentity();
+	temp.setTranslation(getLocalPosition());
+	cbData.worldMatrix *= temp;
+
+	//Add camera transformation
+	Matrix4x4 cameraMatrix = SceneCameraHandler::getInstance()->getSceneCameraViewMatrix();
+	cbData.viewMatrix = cameraMatrix;
+
+	//Perspective View
+	cbData.projMatrix.setPerspectiveFovLH(1.57f, ((float)width / (float)height), 0.1f, 100.0f);
+
+	this->constantBuffer->update(deviceContext, &cbData);
+
+	//SET DEFAULT SHADER IN THE GRAPHICS PIPELINE TO BE ABLE TO DRAW
+	GraphicsEngine::get()->getImmediateDeviceContext()->setVertexShader(vertexShader);
+	GraphicsEngine::get()->getImmediateDeviceContext()->setPixelShader(pixelShader);
+
+	//SET DEFAULT BUFFER IN THE GRAPHICS PIPELINE TO BE ABLE TO DRAW
+	deviceContext->setConstantBuffer(vertexShader, this->constantBuffer);
+	deviceContext->setConstantBuffer(pixelShader, this->constantBuffer);
+
+	deviceContext->setIndexBuffer(this->boxIndexBuffer);
+	deviceContext->setVertexBuffer(this->boxVertexBuffer);
+
+	GraphicsEngine::get()->getImmediateDeviceContext()->setWireframeRenderMode();
+
+	deviceContext->drawIndexedLineList(this->boxIndexBuffer->getSizeIndexList(), 0, 0);
 }
 
 void Pyramid::setAnimSpeed(float speed)
